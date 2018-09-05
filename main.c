368082bdbb4b1223585d4e211e49042e714d0407
Gabriel Domene <domenee.g@gmail.com>
2018-09-05 17:54:39 -0300

    Socket initial configuration

---
+++ main.c
@@ -0,0 +1,51 @@
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+
+int main(){
+
+    //criar socket -> bind -> listen -< connection
+    int socket_name = 0, connfd = 0;
+    struct sockaddr_in serv_addr;
+
+    char sendBuff[1025];
+    time_t ticks;
+
+    socket_name = socket(AF_INET, SOCK_STREAM, 0);
+    if(socket_name == 0)
+    {
+        printf("Failed to create socket, exiting. . . %i\n", socket_name);
+    }else{
+        printf("Socket created!\n");
+    }
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(5000);
+
+    bind(socket_name, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+
+    listen(socket_name, 10);
+
+    while (1)
+    {
+        connfd = accept(socket_name, (struct sockaddr*)NULL, NULL);
+        ticks = time(NULL);
+        snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks));
+        //write(connfd, sendBuff, strlen(sendBuff));
+
+        
+        sleep(1);
+    }
+    close(connfd);
+
+    return 0;
+}
