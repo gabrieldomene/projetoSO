faac51b8acb85ca641df1c271776744d3a597a4c
Gabriel Domene <domenee.g@gmail.com>
2018-09-18 10:31:39 -0300

    Pointer fix and printf buffer flush

--- main.c
+++ main.c
@@ -1,48 +1,44 @@
-#include <string.h>
+#include <strings.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <time.h>
 #include <sys/socket.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <arpa/inet.h>
 
-
-
-
-int main(){
-
-    //criar socket -> bind -> listen -< connection
-    int socket_name = 0, connfd = 0;
+int main()
+{
+    //criar socket -> bind -> listen -> connection
+    int socket_name = 0, new_conn = 0;
     struct sockaddr_in serv_addr;
+    char msg_recv[20];
 
     char sendBuff[1025];
-    time_t ticks;
 
     socket_name = socket(AF_INET, SOCK_STREAM, 0);
-    if(socket_name == 0)
+    if(socket_name < 0)
     {
-        printf("Failed to create socket, exiting. . . %i\n", socket_name);
+        fprintf(stderr, "Failed to create socket, exiting");
     }else{
-        printf("Server socket created!\n");
+
+        fprintf(stderr, "Success");
+        
     }
+    
+    bzero(&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(5000);
-
     bind(socket_name, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
 
     listen(socket_name, 10);
-
-    printf("Esperando msg");
+    
     while (1)
-    {
-
-        connfd = accept(socket_name, (struct sockaddr*)NULL, NULL);
-        sleep(1);
-        close(connfd);
-    }
-
+    {   
+        new_conn = accept(socket_name, (struct sockaddr*)NULL, NULL);
+        read(new_conn, msg_recv, sizeof(msg_recv));
+        fprintf(stderr, "New: %s", msg_recv);
+        close(new_conn);
+     }   
+    
     return 0;
 }
