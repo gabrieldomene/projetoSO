47d2f03df42ca69959bb778ee1ec972e3ea7b027
Gabriel Domene <domenee.g@gmail.com>
2018-09-25 20:04:28 -0300

    Socket cleaning buffer and treating connections

--- main.c
+++ main.c
@@ -3,24 +3,36 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <arpa/inet.h>
+#include <pthread.h>
+#define N_THREAD 100
+/* 
+void *thread_tratadora(void *arg)
+{
+    pthread_t x;
+    x = pthread_self();
+    printf("Nova thread criada. TID = %ld!!\n", x);
+    pthread_exit(0);
+} */
 
 int main()
 {
-    //criar socket -> bind -> listen -> connection
-    int socket_name = 0, new_conn = 0;
+    //criar socket -> bind -> listen -< connection
+    int socket_name = 0, new_conn = 0, option = 1;
     struct sockaddr_in serv_addr;
     char msg_recv[20];
 
     char sendBuff[1025];
 
     socket_name = socket(AF_INET, SOCK_STREAM, 0);
+    setsockopt(socket_name, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option));
     if(socket_name < 0)
     {
-        fprintf(stderr, "Failed to create socket, exiting");
+        printf("%s","Failed to create socket, exiting\n");
     }else{
 
-        fprintf(stderr, "Success");
+        printf("%s", "Created socket!\n");
         
     }
     
@@ -28,15 +40,24 @@ int main()
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(5000);
-    bind(socket_name, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
 
-    listen(socket_name, 10);
-    
+    if (bind(socket_name, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+      perror("ERROR on binding\n");
+      exit(1);
+   }
+
+    if (listen(socket_name, 10)){
+        perror("ERROR on listening\n");
+        exit(1);
+    }
+
     while (1)
     {   
         new_conn = accept(socket_name, (struct sockaddr*)NULL, NULL);
         read(new_conn, msg_recv, sizeof(msg_recv));
-        fprintf(stderr, "New: %s", msg_recv);
+        fprintf(stderr, "Mensagem recebida: %s\n", msg_recv);
+        //apaga resíduo do buffer
+        bzero(msg_recv, sizeof(msg_recv));
         close(new_conn);
      }   
     
