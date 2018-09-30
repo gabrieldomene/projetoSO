0086fd68dc7fe8f48bac831587fdfda3309e2955
Gabriel Domene <domenee.g@gmail.com>
2018-09-30 14:03:45 -0300

    Multiple threads and multiple connections

--- main.c
+++ main.c
@@ -8,25 +8,52 @@
 #include <pthread.h>
 #define N_THREAD 5
 
-void *thread_tratadora(void *arg)
+int con = 0;
+pthread_t threads[N_THREAD];
+
+void *thread_tratadora(void * args)
 {
-    pthread_t x;
-    x = pthread_self();
-    printf("Nova thread criada. TID = %ld!!\n", x);
-    pthread_exit(0);
+    int k, msg;
+    int conn = (int *) args;
+    char msg_recv[1024];
+    printf("\nConection made\n");
+    //Thread roda eterno até sair
+    while(1)
+    {
+        msg = read(conn, msg_recv, sizeof(msg_recv));
+        printf("\n[%i] Conn msg: ", conn);
+        for (k = 0; k < msg; k++){
+            printf("%c",msg_recv[k]); 
+        }
+    }
+    pthread_exit(0); */
 }
+
+void socket_creation();
 
 int main()
 {
-    //criar socket -> bind -> listen -< connection
+    socket_creation();
+    return 0;
+}
+
+//Dúvidas:
+/*
+    Tratar o socket na thread
+    Criar uma threaad ou varias igual o npthreads?
+*/
+
+void socket_creation()
+{
+    //criar socket -> bind -> listen -> connection
     int socket_name = 0, new_conn = 0, option = 1;
     struct sockaddr_in serv_addr;
-    char msg_recv[1024];
-    int k = 0, i = 0;
-    char sendBuff[1025];
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    int i = 0;
+    char sendBuff[1024];
 
     socket_name = socket(AF_INET, SOCK_STREAM, 0);
-    setsockopt(socket_name, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option));
     if(socket_name < 0)
     {
         printf("%s","Failed to create socket, exiting\n");
@@ -35,7 +62,12 @@ int main()
         printf("%s", "Created socket!\n");
         
     }
-    
+
+    if (setsockopt(socket_name, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &option, sizeof(option))) {
+        printf("Socket error");
+        exit(1);
+    }
+    // Cleaning serv_addr
     bzero(&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -46,34 +78,21 @@ int main()
       exit(1);
    }
 
-    if (listen(socket_name, 10)){
+   /*  if (listen(socket_name, 10)){
         perror("ERROR on listening\n");
         exit(1);
-    }
+    } */
 
-    while (1)
-    {   
-        new_conn = accept(socket_name, (struct sockaddr*)NULL, NULL);
-        printf("\nConection made\n");
-        int msg = read(new_conn, msg_recv, sizeof(msg_recv));
-        //printf("Mensagem recebida: %s\n", msg_recv);
-      
-        printf("Mensagem recebida: ");
-        for(k = 0; k < msg; k++){
-            printf("%c", msg_recv[k]);
+    //Running multiple threads
+    for (i = 0; i < N_THREAD; i++) {
+        //Listen capacity
+        if (listen(socket_name, 10)){
+            perror("ERROR on listening\n");
+            exit(1);
         }
-        pthread_t thread[N_THREAD];
-        printf("Antes de criar as thread\n");
-        for (i = 0; i < N_THREAD; i++)
-        {
-            pthread_create(&thread[i], NULL, thread_tratadora, NULL);
-        }
-        for (i = 0; i < N_THREAD; i++)
-        {
-            pthread_join(thread[i], NULL);
-        }
-        close(new_conn);
-     }   
-    
-    return 0;
+        pthread_create(&threads[i], NULL, thread_tratadora, (void *) accept(socket_name, (struct sockaddr *)&address, (socklen_t *)&addrlen));
+	
+    }
+    //close(new_conn);
+        
 }
